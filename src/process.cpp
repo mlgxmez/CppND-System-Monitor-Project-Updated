348db8597ca96e19227cecbaae8a5181d28aa61c
mlgxmez <mlgxmez@gmail.com>
2020-04-27 12:55:09 +0200

    Rearrange objects in comparison operator

--- src/process.cpp
+++ src/process.cpp
@@ -32,5 +32,5 @@ long int Process::UpTime() { return LinuxParser::UpTime(Pid()); }
 // TODO: Overload the "less than" comparison operator for Process objects
 // REMOVE: [[maybe_unused]] once you define the function
 bool Process::operator<(Process const& a) const { 
-    return this->CpuUtilization() < a.CpuUtilization(); 
+    return a.CpuUtilization() < this->CpuUtilization(); 
     }
